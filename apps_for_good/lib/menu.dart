4f5e3cf8e95a2de38705fb844e01c4b7d541501f
888asharma <164044545+888asharma@users.noreply.github.com>
2024-04-03 08:16:11 -0400

    Menu File
    This file contains a skeleton of the hamburger menu.

---
+++ apps_for_good/lib/menu.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+
+class Widget088 extends StatelessWidget {
+  const Widget088({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('Flutter Mapp'),
+    ), 
+    
+    drawer: Drawer(
+      child: ListView(
+        children: [
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 3'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 4'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 5'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 6'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 7'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 8'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 9'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 10'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 11'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 12'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 13'),
+            onTap: () {},
+          ),
+          ListTile( 
+            title: const Text('Item 14'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 15'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    ),
+  );
+}}
