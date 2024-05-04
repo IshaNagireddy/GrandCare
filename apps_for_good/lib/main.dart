e988e1661d38561f60083764f7e17a72be56573c
Isha Nagireddy <nagireddyisha@gmail.com>
2024-05-04 13:22:48 -0400

    merge conflicts

--- apps_for_good/lib/main.dart
+++ apps_for_good/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:GrandCare/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'login_choice.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -17,9 +18,9 @@ class MyApp extends StatelessWidget {
  // This widget is the root of your application.
  @override
  Widget build(BuildContext context) {
-   return MaterialApp(
+   return const MaterialApp(
      debugShowCheckedModeBanner: false,
-     home: LoginChoice(),
+     home: OnBoardingScreen(),
    );
  }
 }
